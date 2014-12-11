5eae49e552e16001a10ec2f5b4388e4240182163
Kielan <ki.lemons@gmail.com>
2014-12-10 20:45:00 -0800

    added some running and touching up. Almost finished jumping

--- Assets/controlScript.cs
+++ Assets/controlScript.cs
@@ -6,16 +6,29 @@ public class controlScript : MonoBehaviour {
 	public float maxSpeed = 10f;
 	bool facingRight = true;
 
+	Animator anim;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator> ();
 	}
 
 	void FixedUpdate() {
 
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+	
+
 		float move = Input.GetAxis ("Horizontal");
 
 		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+		anim.SetBool ("Ground", grounded);
+
+		anim.SetFloat ("Speed", Mathf.Abs (move)); //branch falling animation
 
 		if (move > 0 && !facingRight)
 						Flip ();
@@ -32,6 +45,9 @@ public class controlScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-	
+		if (grounded && Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetBool("Ground", false);
+			rigidbody2D.AddForce(new Vector2(0, 700f));
+		}
 	}
 }
